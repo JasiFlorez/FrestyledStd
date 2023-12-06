c95936e3cd0df44a241e3b82caf482f07f21ee75
eliasNegrete2005 <elias.negrete@etfundacion.org>
2023-12-06 16:22:20 -0500

    todo perfecto

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+import express from "express"
+import dotenv from "dotenv"
+import router from "./routes/router.js";
+import cors from "cors";
+
+dotenv.config();
+const PORTSERVER = process.env.PORTSERVER
+
 const app = express();
-const port = 3000;
 
 
+app.use(cors());
+app.use(express.json());
+app.use("/api",router)
 
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-});
+
+app.listen(PORTSERVER,()=> {
+    console.log(`Servidor corriendo en el puerto ${PORTSERVER}❤️❤️`)
+})
